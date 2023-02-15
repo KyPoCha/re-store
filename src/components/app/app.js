42b63509cde6b035db075f0e1e3da64b25338e53
Kyrylo Polishchuk <poliskyr@fit.cvut.cz>
2023-02-15 19:51:41 +0100

    Add header in main app component

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import Header from "../header";
 import { HomePage, CartPage } from "../pages";
 import "./app.css";
 
 const App = () => {
   return (
     <React.Fragment>
-      <Switch>
-        <Route path="/" component={HomePage} exact />
-        <Route path="/cart" component={CartPage} />
-      </Switch>
+      <main role="main" className="container">
+        <Header numsItems={5} total={210}></Header>
+        <Switch>
+          <Route path="/" component={HomePage} exact />
+          <Route path="/cart" component={CartPage} />
+        </Switch>
+      </main>
     </React.Fragment>
   );
 };
