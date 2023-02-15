895b3d13976fe338bc32b0aebb37b1a45d79a6c2
Kyrylo Polishchuk <poliskyr@fit.cvut.cz>
2023-02-15 19:50:57 +0100

    Add styles for book list component

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -1,15 +1,28 @@
 import React, { Component } from "react";
 import BookListItem from "../book-list-item";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { withBookstoreService } from "../hoc";
+import { bookLoaded } from "../../actions";
+import { compose } from "../../utils";
+
 import "./book-list.css";
 
 class BookList extends Component {
+  componentDidMount() {
+    const { bookstoreService } = this.props;
+    const data = bookstoreService.getBooks();
+
+    this.props.bookLoaded(data);
+  }
+
   render() {
     const { books } = this.props;
     return (
-      <ul>
+      <ul className="book-list">
         {books.map((book) => {
           return (
-            <li>
+            <li key={book.id}>
               <BookListItem book={book} />{" "}
             </li>
           );
@@ -19,4 +32,21 @@ class BookList extends Component {
   }
 }
 
-export default BookList;
+const mapStateToProps = ({ books }) => {
+  return {
+    books,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({ bookLoaded }, dispatch);
+};
+
+export default compose(
+  withBookstoreService(),
+  connect(mapStateToProps, mapDispatchToProps)
+)(BookList);
+
+// export default withBookstoreService()(
+//   connect(mapStateToProps, mapDispatchToProps)(BookList)
+// );
