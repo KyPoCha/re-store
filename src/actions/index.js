ef6e0c805e16599ad665812e4688b477011601f1
Kyrylo Polishchuk <poliskyr@fit.cvut.cz>
2023-02-18 12:08:32 +0100

    Added new features for shopping cart

--- src/actions/index.js
+++ src/actions/index.js
@@ -18,6 +18,27 @@ const bookError = (error) => {
   };
 };
 
+const bookAddedToCart = (bookId) => {
+  return {
+    type: "BOOK_ADDED_TO_CART",
+    payload: bookId,
+  };
+};
+
+const bookRemovedFromCart = (bookId) => {
+  return {
+    type: "BOOK_REMOVED_FROM_CART",
+    payload: bookId,
+  };
+};
+
+const allBooksRemovedFromCart = (bookId) => {
+  return {
+    type: "ALL_BOOKS_REMOVED_FROM_CART",
+    payload: bookId,
+  };
+};
+
 const fetchBooks = (bookstoreService, dispatch) => () => {
   dispatch(bookRequested());
   bookstoreService
@@ -30,4 +51,9 @@ const fetchBooks = (bookstoreService, dispatch) => () => {
     });
 };
 
-export { fetchBooks };
+export {
+  fetchBooks,
+  bookAddedToCart,
+  bookRemovedFromCart,
+  allBooksRemovedFromCart,
+};
