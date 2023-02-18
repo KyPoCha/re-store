690dfa531d2c2a3740b775b00ef2a69b049f70e2
Kyrylo Polishchuk <poliskyr@fit.cvut.cz>
2023-02-18 12:09:35 +0100

    Updated reducers and added new features

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,24 +2,46 @@ const initialState = {
   books: [],
   loading: true,
   error: null,
-  cartItems: [
-    {
-      id: 1,
-      name: "Book1",
-      count: 3,
-      total: 150,
-    },
-    {
-      id: 2,
-      name: "Book2",
-      count: 2,
-      total: 70,
-    },
-  ],
-  orderTotal: 220,
+  cartItems: [],
+  orderTotal: 0,
+};
+
+const updatedCartItems = (cartItems, item, indx) => {
+  if (item.count === 0) {
+    return [...cartItems.slice(0, indx), ...cartItems.slice(indx + 1)];
+  }
+
+  if (indx === -1) {
+    return [...cartItems, item];
+  }
+  return [...cartItems.slice(0, indx), item, ...cartItems.slice(indx + 1)];
+};
+
+const updateCartItem = (book, item = {}, quantity) => {
+  const { id = book.id, count = 0, name = book.name, total = 0 } = item;
+  return {
+    id,
+    name,
+    count: count + quantity,
+    total: total + quantity * book.price,
+  };
+};
+
+const updateOrder = (state, bookId, quantity) => {
+  const { books, cartItems } = state;
+
+  const book = books.find(({ id }) => bookId === id);
+  const itemIndx = cartItems.findIndex(({ id }) => id === bookId);
+  const item = cartItems[itemIndx];
+  const newItem = updateCartItem(book, item, quantity);
+  return {
+    ...state,
+    cartItems: updatedCartItems(cartItems, newItem, itemIndx),
+  };
 };
 
 const reducer = (state = initialState, action) => {
+  console.log(action.type);
   switch (action.type) {
     case "FETCH_BOOKS_REQUEST":
       return {
@@ -41,6 +63,13 @@ const reducer = (state = initialState, action) => {
         loading: false,
         error: action.payload,
       };
+    case "BOOK_ADDED_TO_CART":
+      return updateOrder(state, action.payload, 1);
+    case "BOOK_REMOVED_FROM_CART":
+      return updateOrder(state, action.payload, -1);
+    case "ALL_BOOKS_REMOVED_FROM_CART":
+      const item = state.cartItems.find(({ id }) => id === action.payload);
+      return updateOrder(state, action.payload, -item.count);
     default:
       return state;
   }
